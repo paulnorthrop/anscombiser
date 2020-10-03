7f4a0aaf205a3ea6c0a2d3520986536324760dce
Paul Northrop <p.northrop@ucl.ac.uk>
2020-10-03 09:14:33 +0100

    Get map data

--- R/anscombiser-internal.R
+++ R/anscombiser-internal.R
@@ -14,3 +14,13 @@ NULL
 is_wholenumber <- function(x, tol = .Machine$double.eps^0.5) {
   return(abs(x - round(x)) < tol)
 }
+
+# Function to extract longitude and latitude from the maps package
+
+#' @keywords internal
+#' @rdname anscombiser-internal
+mapdata <- function(region = '.', map = "world", exact = FALSE, ...) {
+  locs <- maps::map(map, region, exact = exact, plot = FALSE, fill = TRUE, ...)
+  locs <- na.omit(data.frame(long = locs$x, lat = locs$y))
+  return(locs)
+}
