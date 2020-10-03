35b333f8e3bfc7a51aa1c38e399c1aff9b051e78
Paul Northrop <p.northrop@ucl.ac.uk>
2020-10-03 07:28:59 +0100

    Initial commit

---
+++ R/get_stats.R
@@ -0,0 +1,49 @@
+#' Calculate Anscombe's summary statistics
+#'
+#' Calculates a particular set of summary statistics for a dataset and returns
+#' them in a form that can be used as the argument \code{stats} in
+#' \code{\link{make_stats}}.
+#'
+#' @param x a numeric matrix or data frame with at least 2 columns/variables.
+#'   Each column contains observations on a different variable.  Missing
+#'   observations are not allowed.
+#' @return A named list of summary statistics containing
+#'   * `n` The sample size.
+#'   * `means` The sample means of each variable.
+#'   * `variances` The sample means of each variable.
+#'   * `correlation` The sample correlation matrix.
+#'   * `intercepts`,`slopes` Matrices whose (i,j)th entries are the estimated
+#'     regression coefficients in a regression of `x[, i]` on `x[, j]`.
+#' @examples
+#' get_stats(anscombe[, c(1, 5)])
+#' @export
+#' @md
+get_stats <- function(x) {
+   if (!is.matrix(x) && !is.data.frame(x)) {
+     stop("x must be a matrix or a dataframe")
+   }
+   if (anyNA(x)) {
+     stop("x must not contain any missing values")
+   }
+   res <- list()
+   res$n <- nrow(x)
+   res$means <- colMeans(x)
+   res$variances <- apply(x, 2, stats::var)
+   res$correlation <- stats::cor(x)
+   nvar <- ncol(x)
+   res$intercepts <- matrix(0, nvar, nvar)
+   res$slopes <- matrix(1, nvar, nvar)
+   for (i in 2:nvar) {
+     for (j in 1:(i - 1)) {
+       coefs <- coef(stats::lm(x[, i] ~ x[, j]))
+       res$intercepts[i, j] <- coefs[1]
+       res$slopes[i, j] <- coefs[2]
+       coefs <- coef(stats::lm(x[, j] ~ x[, i]))
+       res$slopes[j, i] <- coefs[1]
+       res$intercepts[j, i] <- coefs[2]
+     }
+   }
+   dimnames(res$intercepts) <- list(colnames(x), colnames(x))
+   dimnames(res$slopes) <- list(colnames(x), colnames(x))
+   return(res)
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# anscombe
+
+[![Travis-CI Build
+Status](https://travis-ci.org/paulnorthrop/anscombe.svg?branch=master)](https://travis-ci.org/paulnorthrop/anscombe)
+[![AppVeyor Build
+Status](https://ci.appveyor.com/api/projects/status/github/paulnorthrop/anscombe?branch=master&svg=true)](https://ci.appveyor.com/project/paulnorthrop/anscombe)
+[![Coverage
+Status](https://codecov.io/github/paulnorthrop/anscombe/coverage.svg?branch=master)](https://codecov.io/github/paulnorthrop/anscombe?branch=master)
+[![CRAN\_Status\_Badge](https://www.r-pkg.org/badges/version/anscombe)](https://cran.r-project.org/package=anscombe)
+
+## Title
+
+### What does anscombe do?
+
+### An example
+
+### Installation
+
+To get the current released version from CRAN:
+
+``` r
+install.packages("anscombe")
+```
+
+### Vignette
+
+See `vignette("anscombe-vignette", package = "anscombe")` for an
+overview of the package.
