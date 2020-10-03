86c3aecf607e4f883ffb231938e4d7151a16deab
Paul Northrop <p.northrop@ucl.ac.uk>
2020-10-03 13:08:39 +0100

    Option to use set_stats()

--- R/mimic.R
+++ R/mimic.R
@@ -1,12 +1,14 @@
-#' Modify a dataset to achieve certain sample summary statistics
+#' Modify a dataset to achieve certain target sample summary statistics
 #'
 #' Modifies a dataset `x` so that it shares sample summary statistics with
 #' another dataset `x2`.  Alternatively, the summary statistics themselves
 #'
-#' @param x A numeric matrix or data frame.  Each column contains observations
+#' @param x,x2 Numeric matrices or data frames.  Each column contains observations
 #'   on a different variable.  Missing observations are not allowed.
-#' @param x2 A numeric matrix or data frame.  Each column contains observations
-#'   on a different variable.  Missing observations are not allowed.
+#'   \code{\link{get_stats(x2)}} sets the target summary statistics.
+#'   If `x2` is missing then \code{\link{set_stats}} is called with
+#'   `d = ncol(x)` and any additional arguments supplied via `...`.
+#' @param ... Additional arguments to be passed to \code{\link{set_stats}}.
 #' @details The input dataset `x` is modified by shifting, scaling and rotating
 #'   it so that its sample mean and covariance matrix match those of `x2`.
 #' @return A dataset with the same format as `x`.  The returned dataset has the
@@ -29,20 +31,24 @@
 #' }
 #' @export
 #' @md
-mimic <- function(x, x2) {
+mimic <- function(x, x2, ...) {
+  if (missing(x2)) {
+    x2_stats <- set_stats(d = ncol(x), ...)
+  } else {
+    if (!is.matrix(x2) && !is.data.frame(x2)) {
+      stop("x2 must be a matrix or a dataframe")
+    }
+    if (anyNA(x2)) {
+      stop("x2 must not contain any missing values")
+    }
+    x2_stats <- get_stats(x2)
+  }
   if (!is.matrix(x) && !is.data.frame(x)) {
     stop("x must be a matrix or a dataframe")
   }
   if (anyNA(x)) {
     stop("x must not contain any missing values")
   }
-  if (!is.matrix(x2) && !is.data.frame(x2)) {
-    stop("x2 must be a matrix or a dataframe")
-  }
-  if (anyNA(x2)) {
-    stop("x2 must not contain any missing values")
-  }
-  x2_stats <- get_stats(x2)
   new_x <- make_stats(x, x2_stats)
   return(new_x)
 }
