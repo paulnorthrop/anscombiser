218b2cb193791e68fe9865fa444d020564199353
Paul Northrop <p.northrop@ucl.ac.uk>
2020-10-04 13:05:13 +0100

    datasets::

--- tests/testthat/test-anscombise.R
+++ tests/testthat/test-anscombise.R
@@ -2,7 +2,7 @@ context("anscombise")
 
 new_faithful <- anscombise(datasets::faithful, which = 4)
 stats1 <- get_stats(new_faithful)
-stats2 <- get_stats(anscombe[c(4, 8)])
+stats2 <- get_stats(datasets::anscombe[c(4, 8)])
 # Remove the sample sizes because they will be different
 stats1$n <- NULL
 stats2$n <- NULL
--- tests/testthat/test-mimic.R
+++ tests/testthat/test-mimic.R
@@ -1,8 +1,8 @@
 context("mimic")
 
-new_faithful <- mimic(datasets::faithful, anscombe[, c(4, 8)])
+new_faithful <- mimic(datasets::faithful, datasets::anscombe[, c(4, 8)])
 stats1 <- get_stats(new_faithful)
-stats2 <- get_stats(anscombe[c(4, 8)])
+stats2 <- get_stats(datasets::anscombe[c(4, 8)])
 # Remove the sample sizes because they will be different
 stats1$n <- NULL
 stats2$n <- NULL
