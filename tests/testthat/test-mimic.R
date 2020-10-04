1277db20532d34563255584a56180bf99f4d7f18
Paul Northrop <p.northrop@ucl.ac.uk>
2020-10-04 02:41:56 +0100

    Test mimic

--- tests/testthat/test-mimic.R
+++ tests/testthat/test-mimic.R
@@ -1,12 +1,12 @@
-context("anscombise")
+context("mimic")
 
-new_faithful <- anscombise(datasets::faithful, which = 4)
+new_faithful <- mimic(datasets::faithful, anscombe[, c(4, 8)])
 stats1 <- get_stats(new_faithful)
 stats2 <- get_stats(anscombe[c(4, 8)])
 # Remove the sample sizes because they will be different
 stats1$n <- NULL
 stats2$n <- NULL
 
-test_that("anscombise: new faithful and Anscombe 4 has same stats", {
+test_that("mimic: new faithful and Anscombe 4 has same stats", {
   testthat::expect_equivalent(stats1, stats2)
 })
