ae17e5f6c91ff51bd5bbe2591eb6b3166b214778
Paul Northrop <p.northrop@ucl.ac.uk>
2020-10-04 02:46:42 +0100

    Test set_stats()

--- tests/testthat/test-mimic.R
+++ tests/testthat/test-mimic.R
@@ -10,3 +10,15 @@ stats2$n <- NULL
 test_that("mimic: new faithful and Anscombe 4 has same stats", {
   testthat::expect_equivalent(stats1, stats2)
 })
+
+#
+
+new_faithful <- mimic(faithful)
+stats1 <- get_stats(new_faithful)
+mat <- matrix(c(1, 0, 0, 1), 2, 2)
+new_faithful <- mimic(faithful, means = 0, variances = 1, correlation = mat)
+stats2 <- get_stats(new_faithful)
+
+test_that("mimic: new faithful and Anscombe 4 has same stats", {
+  testthat::expect_equal(stats1, stats2)
+})
