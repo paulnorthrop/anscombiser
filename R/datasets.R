f1fd43c92f7d23b9281110d6238c2d5d769e6168
Paul Northrop <p.northrop@ucl.ac.uk>
2022-10-01 17:13:04 +0100

    Add Anscombe's datasets separated

---
+++ R/datasets.R
@@ -0,0 +1,44 @@
+#' Anscombe's Quartet Separated
+#'
+#' Provides Anscombe's Quartet as separate data frames.
+#'
+#' @source Anscombe's Quartet of 'Identical' Simple Linear Regressions:
+#'   [`anscombe`][`datasets::anscombe`] in the
+#'   [`datasets`][`datasets::datasets-package`] package
+#' @name anscombe
+#' @md
+NULL
+
+#' @format `anscombe1`: an object of class `data.frame` with 11 rows and 2
+#'   columns (columns 1 and 5 of [`anscombe`][`datasets::anscombe`]).
+#' @rdname anscombe
+#' @md
+"anscombe1"
+
+#' @format `anscombe2`: an object of class `data.frame` with 11 rows and 2
+#'   columns (columns 2 and 6 of [`anscombe`][`datasets::anscombe`]).
+#' @rdname anscombe
+#' @md
+"anscombe2"
+
+#' @format `anscombe3`: an object of class `data.frame` with 11 rows and 2
+#'   columns (columns 3 and 7 of [`anscombe`][`datasets::anscombe`]).
+#' @rdname anscombe
+#' @md
+"anscombe3"
+
+#' @format `anscombe4`: an object of class `data.frame` with 11 rows and 2
+#'   columns (columns 4 and 8 of [`anscombe`][`datasets::anscombe`]).
+#' @rdname anscombe
+#' @md
+"anscombe4"
+
+#' Donald Trump
+#'
+#' A dataset that provides an image of Donald Trump's face.
+#'
+#' @format A matrix with 4885 rows and 2 columns: `x` and `y`.
+#' @source This image was created by Accentaur from the Noun Project.
+#' \url{https://thenounproject.com/term/donald-trump/727774/}
+#' @md
+"trump"
