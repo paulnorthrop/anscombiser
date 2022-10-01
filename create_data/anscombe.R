8aa513e8c040fa89c850c7422497ef744beb3a89
Paul Northrop <p.northrop@ucl.ac.uk>
2022-10-01 17:13:47 +0100

    Create Anscombe's datasets separately

---
+++ create_data/anscombe.R
@@ -0,0 +1,11 @@
+# Create anscombe1, anscombe2, anscomb3 and anscombe4 for convenience
+
+anscombe1 <- datasets::anscombe[, c(1, 5)]
+anscombe2 <- datasets::anscombe[, c(2, 6)]
+anscombe3 <- datasets::anscombe[, c(3, 7)]
+anscombe4 <- datasets::anscombe[, c(4, 8)]
+
+use_data(anscombe1)
+use_data(anscombe2)
+use_data(anscombe3)
+use_data(anscombe4)
