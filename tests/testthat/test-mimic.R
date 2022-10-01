55dbc2cea057d60be26ae84b9dd6c17dde81b03d
Paul Northrop <p.northrop@ucl.ac.uk>
2022-10-01 12:52:43 +0100

    Tidy the text

--- tests/testthat/test-mimic.R
+++ tests/testthat/test-mimic.R
@@ -1,5 +1,7 @@
 #context("mimic")
 
+# Old Faithful data mimicking Anscombe 4
+
 new_faithful <- mimic(datasets::faithful, datasets::anscombe[, c(4, 8)])
 stats1 <- get_stats(new_faithful)
 stats2 <- get_stats(datasets::anscombe[c(4, 8)])
@@ -7,11 +9,11 @@ stats2 <- get_stats(datasets::anscombe[c(4, 8)])
 stats1$n <- NULL
 stats2$n <- NULL
 
-test_that("mimic: new faithful and Anscombe 4 has same stats", {
+test_that("mimic: new faithful and Anscombe 4 have same stats", {
   testthat::expect_equal(stats1, stats2, ignore_attr = TRUE)
 })
 
-#
+# New version of Old Faithful with zero means, unit variances, zero correlation
 
 new_faithful <- mimic(faithful)
 stats1 <- get_stats(new_faithful)
@@ -19,6 +21,6 @@ mat <- matrix(c(1, 0, 0, 1), 2, 2)
 new_faithful <- mimic(faithful, means = 0, variances = 1, correlation = mat)
 stats2 <- get_stats(new_faithful)
 
-test_that("mimic: new faithful and Anscombe 4 has same stats", {
+test_that("mimic: new faithful has the required stats", {
   testthat::expect_equal(stats1, stats2)
 })
