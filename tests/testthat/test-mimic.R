bb48e22538fbb8b17fa31eec271a799e381aaab4
Paul Northrop <p.northrop@ucl.ac.uk>
2022-10-01 18:14:37 +0100

    Add test of 3D example

--- tests/testthat/test-mimic.R
+++ tests/testthat/test-mimic.R
@@ -42,6 +42,8 @@ test_that("mimic: new faithful has the required stats, idempotent = FALSE", {
 
 # Check that idempotent = TRUE achieves idempotency
 
+## Old Faithful (2D)
+
 new_faithful <- mimic(datasets::faithful, datasets::faithful)
 stats1 <- get_stats(new_faithful)
 stats2 <- get_stats(datasets::faithful)
@@ -53,4 +55,19 @@ test_that("mimic: idempotent = TRUE, stats agree", {
 test_that("mimic: idempotent = TRUE, datasets agree", {
   testthat::expect_equal(as.matrix(new_faithful),
                          as.matrix(datasets::faithful), ignore_attr = TRUE)
+})
+
+## Diameter, Height and Volume for Black Cherry Trees (3D)
+
+new_trees <- mimic(datasets::trees, datasets::trees)
+stats1 <- get_stats(new_trees)
+stats2 <- get_stats(datasets::trees)
+
+test_that("mimic: idempotent = TRUE, stats agree", {
+  testthat::expect_equal(stats1, stats2, ignore_attr = TRUE)
+})
+# Use as.matrix to deal with the differences in class
+test_that("mimic: idempotent = TRUE, datasets agree", {
+  testthat::expect_equal(as.matrix(new_trees),
+                         as.matrix(datasets::trees), ignore_attr = TRUE)
 })
